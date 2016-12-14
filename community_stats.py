7a44dbfbbd31e972a4dd1d460f066955755d9004
Tim Hausler <thausler@pivotal.io>
2016-12-14 08:58:37 -0800

    Pages through and collects github issues

--- community_stats.py
+++ community_stats.py
@@ -1,19 +1,31 @@
 import requests
 import sys
 import json
+import os
 
 owner = sys.argv[1]
 repo = sys.argv[2]
 interval = 7
 range = 30
 
+def github_creds():
+    requests.auth.HTTPBasicAuth(os.environ['COMM_USER'], os.environ['COMM_PASSWORD'])
+
+
 def fetch_issue_events(owner, repo, interval, range):
-    req = requests.get('https://api.github.com/search/issues?q=the+type:pr+sort:created_at', auth=requests.auth.HTTPBasicAuth('communitystats', 'clone7adhere'))
-    req = requests.get(('https://api.github.com/repos/%(owner)s/%(repo)s/issues?sort:created_at' % locals()), auth=requests.auth.HTTPBasicAuth('communitystats', 'clone7adhere'))
-    while req.links['next']['url']
-    print req.text
-    issues = json.loads(req.text)
-    map(get_events, issues)
+    req = requests.get('https://api.github.com/search/issues?q=the+type:pr+sort:created_at', auth=github_creds())
+    req = requests.get(('https://api.github.com/repos/%(owner)s/%(repo)s/issues?sort:created_at' % locals()), auth=github_creds())
+    print req.links
+    issue_array = json.loads(req.text)
+    while ('next' in req.links and 'url' in req.links['next']):
+        print req.links['next']['url']
+        #print req.text
+        req = requests.get(req.links['next']['url'], auth=github_creds())
+        issue_array += json.loads(req.text)
+        print req.links
+        if not ('next' in req.links and 'url' in req.links['next']):
+            break
+    map(get_events, issue_array)
 
 def get_events(issue):
     print issue['created_at']
