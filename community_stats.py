1d513e340fd56534717de018b3be58a0b7dcef84
Tim Hausler <thausler@pivotal.io>
2016-12-29 09:21:32 -0800

    expand range of stats files created

--- community_stats.py
+++ community_stats.py
@@ -41,8 +41,17 @@ class CommunityWeekFinder:
             self.community_week_dict[opened_week_key].issues_opened += 1
         closed_week_key = get_sunday_before_day(issue_event.issue_closed)
         if closed_week_key:
-            print "adding closure to week " + str(self.community_week_dict[closed_week_key])
             self.community_week_dict[closed_week_key].issues_closed += 1
+        self.add_latencies(issue_event)
+
+    def add_latencies(self, issue_event):
+        opened_week_key = get_sunday_before_day(issue_event.issue_opened)
+        closed_week_key = get_sunday_before_day(issue_event.issue_closed) or datetime.datetime.max
+        if opened_week_key:
+            week_key = opened_week_key
+            while (self.community_week_dict[week_key] and week_key <= closed_week_key):
+                self.community_week_dict[week_key].issues_open += 1
+                self.community_week_dict[week_key].total_time_open += week_key - get_sunday_before_day(issue_event.issue_opened)
 
 class CommunityWeek:
     issues_opened = 0
@@ -50,16 +59,22 @@ class CommunityWeek:
     start_date = 0
     end_date = 0
 
+    issues_open = 0
+    total_time_open = 0
+
     def __init__(self, start_date):
-        print "making week " + str(start_date)
         self.start_date = start_date
         end_date = start_date + relativedelta(weeks=+1)
         issues_opened = 0
         issues_closed = 0
+        issues_open = 0
+        total_time_open = 0
 
     def contains_date(date):
         return date >= start_date and date < end_date
 
+    def average_issue_age():
+        return issues_open / total_time_open
 
 def github_creds():
     return requests.auth.HTTPBasicAuth(os.environ['COMM_USER'], os.environ['COMM_PASSWORD'])
@@ -107,24 +122,56 @@ week_finder = CommunityWeekFinder(start_date, end_date)
 
 map(week_finder.add_event, events)
 
-for week in sorted(week_finder.community_week_dict.values(), key=get_date):
-    print "Week " + str(week.start_date) + " had issues opened: " + str(week.issues_opened) + ", issues closed: " + str(week.issues_closed)
+#unaggregated statistics
 
 velocities = open('point_velocities.csv', 'w')
 velocities.truncate()
 
+still_open = open('still_open.csv', 'w')
+still_open.truncate()
+
 burnup = open('burnup.csv', 'w')
+burnup.truncate()
 
 week_index = 0
 total_opened = 0
 total_closed = 0
 difference = 0
+
 velocities.write('week,opened,closed\n')
+still_open.write('week,still_opened\n')
 burnup.write('week,opened,closed\n')
+
 for week in sorted(week_finder.community_week_dict.values(), key=get_date):
     total_opened += week.issues_opened
     total_closed += week.issues_closed
     difference = total_opened - total_closed
+
     velocities.write(str(week_index) + ',' + str(week.issues_opened) + ','+ str(week.issues_closed) + '\n')
-    burnup.write(str(week_index) + ',' + str(difference) + '\n')#str(total_opened) + ','+ str(total_closed) + '\n')
+    still_open.write(str(week_index) + ',' + str(difference) + '\n')
+    burnup.write(str(week_index) + ',' + str(total_opened) + ',' + str(total_closed) + '\n')
     week_index += 1
+
+#aggregated statistics
+
+month_velocities = open('month_velocities.csv', 'w')
+month_velocities.truncate()
+
+month_index = 0
+week_index = 0
+opened_per_month = 0
+closed_per_month = 0
+
+
+burnup.write('month,opened,closed\n')
+
+for week in sorted(week_finder.community_week_dict.values(), key=get_date):
+    opened_per_month += week.issues_opened
+    closed_per_month += week.issues_closed
+    if week_index % 4 == 3:
+        month_velocities.write(str(month_index) + ',' + str(opened_per_month) + ','+ str(closed_per_month) + '\n')
+        month_index += 1
+        opened_per_month = 0
+        closed_per_month = 0
+    week_index += 1
+
