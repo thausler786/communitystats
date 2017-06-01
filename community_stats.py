980d1f2ceb5c686f3369e1ab061408d889f9830d
Tim Hausler <thausler@pivotal.io>
2017-06-01 08:49:22 -0700

    Move CommunityWeek business logic into new module

--- community_stats.py
+++ community_stats.py
@@ -5,6 +5,7 @@ import requests
 import sys
 import json
 import os
+from community_week import CommunityWeekFinder, CommunityWeek
 
 owner = sys.argv[1]
 repo = sys.argv[2]
@@ -17,69 +18,6 @@ class IssueEvents:
     def __init__(self, issue_opened, issue_closed):
         self.issue_opened = issue_opened
         self.issue_closed = issue_closed
-
-class CommunityWeekFinder:
-    start_date = 0
-    end_date = 0
-    community_week_dict = {}
-
-    def __init__(self, start_date, end_date):
-        self.start_date = start_date
-        self.end_date = end_date
-        week_start_date = start_date
-        week_index = 0
-        while week_start_date < end_date:
-            self.community_week_dict[week_start_date] = CommunityWeek(week_start_date, week_index)
-            week_start_date = week_start_date + relativedelta(weeks=+1)
-            week_index += 1
-
-    def get_week(date):
-        return date
-
-    def add_event(self, issue_event):
-        opened_week_key = get_sunday_before_day(issue_event.issue_opened)
-        if opened_week_key:
-            self.community_week_dict[opened_week_key].issues_opened += 1
-        closed_week_key = get_sunday_before_day(issue_event.issue_closed)
-        if closed_week_key:
-            self.community_week_dict[closed_week_key].issues_closed += 1
-        self.add_latencies(issue_event)
-
-    def add_latencies(self, issue_event):
-        opened_week_key = get_sunday_before_day(issue_event.issue_opened)
-        closed_week_key = get_sunday_before_day(issue_event.issue_closed) or datetime.date.max
-        opened_week_index = self.community_week_dict[opened_week_key].week_index
-
-        if opened_week_key:
-            week_key = opened_week_key
-            while (week_key in self.community_week_dict and week_key <= closed_week_key):
-                current_week = self.community_week_dict[week_key]
-                current_week.issues_open += 1
-                current_week.total_weeks_open += current_week.week_index - opened_week_index
-                week_key = week_key + datetime.timedelta(days=+7)
-
-class CommunityWeek:
-    issues_opened = 0
-    issues_closed = 0
-    start_date = 0
-    end_date = 0
-
-    issues_open = 0
-    total_weeks_open = 0
-
-    week_index = 0
-
-    def __init__(self, start_date, week_index):
-        self.start_date = start_date
-        end_date = start_date + relativedelta(weeks=+1)
-        issues_opened = 0
-        issues_closed = 0
-        issues_open = 0
-        total_weeks_open = 0
-        self.week_index = week_index
-
-    def average_issue_age(self):
-        return float(self.total_weeks_open) / float(self.issues_open)
 
 def github_creds():
     return requests.auth.HTTPBasicAuth(os.environ['COMM_USER'], os.environ['COMM_PASSWORD'])
@@ -99,10 +37,6 @@ def get_events(issue):
     events = IssueEvents(get_day_for_date(issue['created_at']), get_day_for_date(issue['closed_at']))
     return events
 
-def get_sunday_before_day(day):
-    if day is None:
-        return None
-    return day + relativedelta(weekday=SU(-1))
 
 def get_day_for_date(date_string):
     if date_string is None:
---
+++ community_week.py
@@ -0,0 +1,77 @@
+from dateutil.relativedelta import *
+from dateutil.parser import *
+import datetime
+import requests
+import sys
+import json
+import os
+
+class CommunityWeekFinder:
+    start_date = 0
+    end_date = 0
+    community_week_dict = {}
+
+    def __init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
+        week_start_date = start_date
+        week_index = 0
+        while week_start_date < end_date:
+            self.community_week_dict[week_start_date] = CommunityWeek(week_start_date, week_index)
+            week_start_date = week_start_date + relativedelta(weeks=+1)
+            week_index += 1
+
+    def get_week(date):
+        return date
+
+    def add_event(self, issue_event):
+        opened_week_key = CommunityWeekFinder.get_sunday_before_day(issue_event.issue_opened)
+        if opened_week_key:
+            self.community_week_dict[opened_week_key].issues_opened += 1
+        closed_week_key = CommunityWeekFinder.get_sunday_before_day(issue_event.issue_closed)
+        if closed_week_key:
+            self.community_week_dict[closed_week_key].issues_closed += 1
+        self.add_latencies(issue_event)
+
+    def add_latencies(self, issue_event):
+        opened_week_key = CommunityWeekFinder.get_sunday_before_day(issue_event.issue_opened)
+        closed_week_key = CommunityWeekFinder.get_sunday_before_day(issue_event.issue_closed) or datetime.date.max
+        opened_week_index = self.community_week_dict[opened_week_key].week_index
+
+        if opened_week_key:
+            week_key = opened_week_key
+            while (week_key in self.community_week_dict and week_key <= closed_week_key):
+                current_week = self.community_week_dict[week_key]
+                current_week.issues_open += 1
+                current_week.total_weeks_open += current_week.week_index - opened_week_index
+                week_key = week_key + datetime.timedelta(days=+7)
+
+    @staticmethod
+    def get_sunday_before_day(day):
+        if day is None:
+            return None
+        return day + relativedelta(weekday=SU(-1))
+
+class CommunityWeek:
+    issues_opened = 0
+    issues_closed = 0
+    start_date = 0
+    end_date = 0
+
+    issues_open = 0
+    total_weeks_open = 0
+
+    week_index = 0
+
+    def __init__(self, start_date, week_index):
+        self.start_date = start_date
+        end_date = start_date + relativedelta(weeks=+1)
+        issues_opened = 0
+        issues_closed = 0
+        issues_open = 0
+        total_weeks_open = 0
+        self.week_index = week_index
+
+    def average_issue_age(self):
+        return float(self.total_weeks_open) / float(self.issues_open)
+
